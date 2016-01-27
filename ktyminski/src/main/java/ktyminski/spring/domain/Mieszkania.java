cc21db176390646817a004c20fb4846ee06e0b9d
karoltyminski <karoltyminski@ktyminski>
2016-01-27 19:25:40 +0100

    Stworzenie modeli

---
+++ ktyminski/src/main/java/ktyminski/spring/domain/Mieszkania.java
@@ -0,0 +1,87 @@
+package ktyminski.spring.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+
+import ktyminski.spring.domain.Wynajmujacy;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "Mieszkania.wszystkie", query = "Select m from Mieszkania m"),
+	
+})
+public class Mieszkania {
+
+	private Long id;
+ 	private Wynajmujacy wynajmujacy;
+	private String ulica = "";
+	private Integer cena = 0;
+	private String opis = "";
+
+	 public Mieszkania() {
+	}
+
+	public Mieszkania(Wynajmujacy wynajmujacy, String ulica , Integer cena, String opis)
+	{
+	this.wynajmujacy = wynajmujacy;
+	this.ulica = ulica;
+	this.cena = cena;
+	this.opis = opis;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "id_wynajmujacy", nullable = false)
+	
+	public Wynajmujacy getWynajmujacy() {
+	return wynajmujacy;
+	}
+	public void setWynajmujacy(Wynajmujacy wynajmujacy) { this.wynajmujacy=wynajmujacy; }
+	
+	
+	public String getUlica() {
+		return ulica;
+	}
+	public void setUlica(String ulica) {
+		this.ulica = ulica;
+	}
+
+
+	public Integer getCena() {
+		return cena;
+	}
+	public void setCena(Integer cena) {
+		this.cena = cena;
+	}
+
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+}
+
+
---
+++ ktyminski/src/main/java/ktyminski/spring/domain/Wynajmujacy.java
@@ -0,0 +1,84 @@
+package ktyminski.spring.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+
+import ktyminski.spring.domain.*;
+
+@Entity
+@NamedQueries({ 
+	@NamedQuery(name = "wynajmujacy.wszystkie", query = "Select w from Wynajmujacy w"),
+
+})
+public class Wynajmujacy {
+	
+	private Long id;
+	private String imie = "";
+	private String nazwisko = "";
+	private Long pesel;
+
+	private List<Mieszkania> mieszkania = new ArrayList<Mieszkania>();
+
+
+	public Wynajmujacy() {
+	}
+	
+	public Wynajmujacy(Long id, String imie, String nazwisko, Long pesel) {
+	this.imie = imie;
+	this.nazwisko = nazwisko;
+	this.pesel = pesel;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	@OneToMany(mappedBy = "wynajmujacy", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	public List<Mieszkania> getMieszkania() {
+	return mieszkania;
+	}
+	public void setMieszkania(List<Mieszkania> mieszkania) {
+	this.mieszkania = mieszkania;
+	}
+
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+
+	
+	public String getNazwisko() {
+		return nazwisko;
+	}
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+
+
+	public Long getPesel() {
+		return pesel;
+	}
+	public void setPesel(Long pesel) {
+		this.pesel = pesel;
+	}
+
+}
